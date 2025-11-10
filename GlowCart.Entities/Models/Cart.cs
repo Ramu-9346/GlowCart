9f3b906786f3d88fa943056b63858824c7424108
Ramu-9346 <ramurunja91@gmail.com>
2025-11-10 18:59:45 +0530

    Created checkout functionality

--- GlowCart.Entities/Models/Cart.cs
+++ GlowCart.Entities/Models/Cart.cs
@@ -3,7 +3,6 @@
     public class Cart
     {
         public int CartId { get; set; }
-        public int UserId { get; set; }
         public int ProductId { get; set; }
         public string? ProductName { get; set; }
         public decimal Price { get; set; }
