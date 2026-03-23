dcb5f613461dc36b405eb1b54a4e673b69c1cabe
RAM <ramurunja91@gmail.com>
2026-03-23 22:04:08 +0530

    fix(ERROR): Login button is unresponsive on Android devices.

---
+++ GlowCart.UI/Views/LoginPage.xaml.cs
@@ -0,0 +1,20 @@
+using System;
+using Xamarin.Forms;
+
+namespace GlowCart.UI.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            loginButton.Clicked += OnLoginButtonClicked;
+        }
+
+        private async void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            // Handle login logic here
+            await DisplayAlert("Login", "Login button clicked.", "OK");
+        }
+    }
+}
