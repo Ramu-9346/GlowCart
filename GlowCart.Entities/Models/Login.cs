d52522f80437254715b0cb6951a35ea38b6bb859
RAM <ramurunja91@gmail.com>
2026-06-26 14:24:20 +0530

    fix(ERROR): The login and registration pages are working too slow.

--- GlowCart.Entities/Models/Login.cs
+++ GlowCart.Entities/Models/Login.cs
@@ -1,13 +1,1 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace GlowCart.Entities.Models
-{
-    public class Login
-    {
-        [Required, EmailAddress]
-        public string? Email { get; set; }
-
-        [Required, DataType(DataType.Password)]
-        public string? Password { get; set; }
-    }
-}
+N/A
